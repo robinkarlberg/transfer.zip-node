ab4999d0fdb9aee8de46b89e20de606a6d425fc6
Robin Karlberg <karrob@protonmail.ch>
2025-07-10 18:36:28 +0200

    caddy prepare

---
+++ poor-mans-cloud-init.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# This script installs docker, I don't want to run all this manually when setting up new nodes
+# and same is probably true for you :)
+
+sudo apt-get update
+
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker run hello-world
