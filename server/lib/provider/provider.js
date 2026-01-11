97c9f13731c6a598958df67f4d478a1a4c09a07b
Robin Karlberg <karrob@protonmail.ch>
2026-01-11 19:25:41 +0100

    option to disable legacy provider

--- server/lib/provider/provider.js
+++ server/lib/provider/provider.js
@@ -4,7 +4,8 @@ import { S3Provider } from "./S3Provider.js";
 
 // legacy provider to facilitate downloads of old v1 transfers
 // TODO: remove this when pushing to stable self-host (main) branch
-export const legacyProvider = new S3Provider(conf.providers[conf.backend_v1_temp_dl])
+console.log("backend_v1_temp_dl", conf.backend_v1_temp_dl ? "ENABLED" : "DISABLED")
+export const legacyProvider = !!conf.backend_v1_temp_dl ? new S3Provider(conf.providers[conf.backend_v1_temp_dl]) : null
 
 export const provider = conf.active == "local" ?
   new LocalProvider(conf.providers["local"])
